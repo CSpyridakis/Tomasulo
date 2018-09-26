7e5c0e7fe1849a4bd11e620b6e47118cbf4d8860
Spyridakis Christos <spyridakischristos@gmail.com>
2018-09-26 23:28:08 +0300

    Basic modules added (5 and 32 bits)

---
+++ src/RF/Reg_32bits.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company/University:        Technical University of Crete (TUC) - GR
+-- Engineer:                  Spyridakis Christos 
+--                            Bellonias Panagiotis
+-- 
+-- Create Date:                
+-- Design Name: 	 
+-- Module Name:               Reg_32bits - Behavioral 
+-- Project Name:              Tomasulo
+-- Target Devices:            NONE
+-- Tool versions:             Xilinx ISE 14.7 --TODO: VIVADO
+-- Description:               Introduction in Dynamic Instruction Scheduling (Advanced Computer Architecture)
+--                            implementing Tomasulo's Algorithm 	 
+--
+-- Dependencies:              NONE
+--
+-- Revision:                  0.01
+-- Revision                   0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Reg_32bits is
+    Port ( CLK  : in  STD_LOGIC;
+           RST  : in  STD_LOGIC;
+           EN   : in  STD_LOGIC;
+           INN  : in  STD_LOGIC_VECTOR (31 downto 0);
+           OUTT : out  STD_LOGIC_VECTOR (31 downto 0));
+end Reg_32bits;
+
+architecture Behavioral of Reg_32bits is
+
+begin
+
+   process(CLK,RST)
+	begin
+	   if (RST='1') then                 --RST
+	         OUTT<="00000000000000000000000000000000";
+		elsif (rising_edge(CLK)) then
+			if (EN='1') then               --Write Enable
+				OUTT<=INN;
+			end if;
+		end if;
+   end process;
+	
+end Behavioral;
+
