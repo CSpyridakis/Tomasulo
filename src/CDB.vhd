67aa7e315eec72ddc99d3ac6389bd3bcf8f4f0e9
0xp374 <belloniasp@gmail.com>
2018-10-27 17:21:00 +0300

    Add files via upload
    CDB v2

--- src/CDB.vhd
+++ src/CDB.vhd
@@ -1,41 +1,47 @@
 ----------------------------------------------------------------------------------
--- Company/University:        Technical University of Crete (TUC) - GR
--- Engineer:                  Spyridakis Christos 
---                            Bellonias Panagiotis
+-- Company: 
+-- Engineer: 
 -- 
--- Create Date:                
--- Design Name:    
--- Module Name:               CDB - Behavioral 
--- Project Name:              Tomasulo
--- Target Devices:            NONE
--- Tool versions:             Xilinx ISE 14.7 --TODO: VIVADO
--- Description:               Introduction in Dynamic Instruction Scheduling (Advanced Computer Architecture)
---                            implementing Tomasulo's Algorithm    
+-- Create Date:    20:17:20 10/18/2018 
+-- Design Name: 
+-- Module Name:    CDB - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
 --
--- Dependencies:              NONE
+-- Dependencies: 
 --
--- Revision:                  0.01
--- Revision                   0.01 - File Created
+-- Revision: 
+-- Revision 0.01 - File Created
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity CDB is
-    Port ( AR_REQ : in  STD_LOGIC;
-           AR_D : in  STD_LOGIC_VECTOR (31 downto 0);
-           ARD_Q : in  STD_LOGIC_VECTOR (3 downto 0);
-           L_REQ : in  STD_LOGIC;
-           L_D : in  STD_LOGIC_VECTOR (31 downto 0);
-           LRD_Q : in  STD_LOGIC_VECTOR (3 downto 0);
+    Port ( A_REQUEST : in  STD_LOGIC;
+           A_V : in  STD_LOGIC_VECTOR (31 downto 0);
+           A_Q : in  STD_LOGIC_VECTOR (4 downto 0);
+           L_REQUEST : in  STD_LOGIC;
+           L_V : in  STD_LOGIC_VECTOR (31 downto 0);
+           L_Q : in  STD_LOGIC_VECTOR (4 downto 0);
            RST : in  STD_LOGIC;
            CLK : in  STD_LOGIC;
-           GRANTED : out  STD_LOGIC_VECTOR (1 downto 0);
+			  A_GRAND : out STD_LOGIC;
+			  L_GRAND : out STD_LOGIC;
            CDB_V : out  STD_LOGIC_VECTOR (31 downto 0);
-           CDB_Q : out  STD_LOGIC_VECTOR (3 downto 0));
+           CDB_Q : out  STD_LOGIC_VECTOR (4 downto 0));
 end CDB;
 
 architecture Behavioral of CDB is
@@ -49,20 +55,20 @@ begin
         if(RST='1') then
             CDB_GRANTED <= "11"; --CDB OFF
             LAST <= "01";	 -- Proteraiotita se logical	
-        elsif(AR_REQ = '1' and L_REQ = '1')then
+        elsif(A_REQUEST = '1' and L_REQUEST = '1')then
             if(LAST="01")then -- Paw logical
-                CDB_GRANTED <= "00";
-                LAST <= "00";		
+                CDB_GRANTED <= "10";
+                LAST <= "10";		
             else
                 CDB_GRANTED <= "01";
                 LAST <= "01";	
             end if;
-        elsif(AR_REQ = '1')then
+        elsif(A_REQUEST = '1')then
             CDB_GRANTED <= "01";
             LAST <= "01";		
-        elsif(L_REQ = '1')then
-            CDB_GRANTED <= "00";
-            LAST <= "00";		
+        elsif(L_REQUEST = '1')then
+            CDB_GRANTED <= "10";
+            LAST <= "10";		
         else       
             CDB_GRANTED <= "11";
         end if;        
@@ -70,15 +76,16 @@ begin
     end process;
 	
 	OUTPUT <= CDB_GRANTED;
+	A_GRAND <= CDB_GRANTED(0);
+	L_GRAND <= CDB_GRANTED(1);
 	
-	GRANTED <= CDB_GRANTED;
 	
-	CDB_V <= 	AR_D WHEN OUTPUT = "01" ELSE
-			L_D WHEN OUTPUT = "00" ELSE
+	CDB_V <= 	A_V WHEN OUTPUT = "01" ELSE
+			L_V WHEN OUTPUT = "10" ELSE
 			(OTHERS => '0');
 				
-	CDB_Q <=	ARD_Q WHEN OUTPUT = "01" ELSE
-			LRD_Q WHEN OUTPUT = "00" ELSE
+	CDB_Q <=	A_Q WHEN OUTPUT = "01" ELSE
+			L_Q WHEN OUTPUT = "10" ELSE
 			(OTHERS => '1');
  
 
